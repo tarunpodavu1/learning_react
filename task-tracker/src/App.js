ab00166ad9ce87a7cc45934fef9b9806f9c613a4
trX-X <tarinkumarpodavu4@gmail.com>
2021-12-26 23:12:01 +0530

    Task Tracker app in react

---
+++ task-tracker/src/App.js
@@ -0,0 +1,53 @@
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30 p.m",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30 p.m",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30 p.m",
+      reminder: false,
+    },
+  ]);
+
+  //Deleting task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle Reminder true/fasle
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  return (
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks left to show"
+      )}
+    </div>
+  );
+}
+
+export default App;
