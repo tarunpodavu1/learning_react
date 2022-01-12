eadde90fccdeb7a16f5c1cff46379e1eaf671e5c
trX-X <tarinkumarpodavu4@gmail.com>
2022-01-13 00:25:52 +0530

    Class based components practice

---
+++ class-based-example/src/components/UserFinder.js
@@ -0,0 +1,72 @@
+import { Fragment, useState, useEffect, Component } from "react";
+import UsersContext from "../store/user-context";
+import ErrorBoundary from "./ErrorBoundary";
+import classes from "./UserFinder.module.css";
+
+import Users from "./Users";
+
+class UserFinder extends Component {
+  static contextType = UsersContext;
+
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: "",
+    };
+  }
+  componentDidMount() {
+    // Send http request...
+    this.setState({ filteredUsers: this.context.users });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
+      </>
+    );
+  }
+}
+
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <div className={classes.finder}>
+//       <input type="search" onChange={searchChangeHandler} />
+//       <Users users={filteredUsers} />
+//     </div>
+//   );
+// };
+
+export default UserFinder;
