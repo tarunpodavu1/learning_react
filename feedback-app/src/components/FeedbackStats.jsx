5edde300e6563fed6fcfd450b088d619ac9818cf
trX-X <tarinkumarpodavu4@gmail.com>
2022-01-25 20:47:22 +0530

    Updated the project

--- feedback-app/src/components/FeedbackStats.jsx
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react/cjs/react.development';
+import { useContext } from 'react';
 import FeedbackContext from '../context/FeedbackContext';
 
 const FeedbackStats = () => {
