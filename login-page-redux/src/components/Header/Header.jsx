3a29a80f12d652f90bfb27fd57362aefa0c2628b
trX-X <tarinkumarpodavu4@gmail.com>
2022-03-30 17:08:07 +0530

    Updating system

---
+++ login-page-redux/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Navigation from './Navigation';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>A Page</h1>
+      <Navigation />
+    </header>
+  );
+};
+export default Header;
---
+++ login-page-redux/src/components/Header/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Users</a>
+        </li>
+
+        <li>
+          <a href="/">Admin</a>
+        </li>
+
+        <li>
+          <button>Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navigation;
